e2dc947803d39b0736ce9a54f468c86c60cdffff
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 13:26:02 +0530

    conversation

--- just.sh
+++ just.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-
+############################################################################
 # read -p 'Enter the name : ' name
 
 # echo "The name you entered is : $name "
-
+##############################################################################
 # read -p "enter number1 : " number1
 # read -p "enter number2 : " number2
 
 # total=$(($number1 + $number2))
 
 # echo " Sum of 2 numbers is : $total "
+################################################################################3
 
+# read -p "Enter which city you live in : " city
 
-read -p "Enter which city you live in : " city
+# echo " Oh great, You live in $city"
+###################################################################################3
 
-echo " Oh great, You live in $city"
+read -p "Enter a number : " number
+
+if
+    echo $(($number/2))
+    echo "The number $number you entered is even"
+else   
+    echo "The number $number you entered is odd"
