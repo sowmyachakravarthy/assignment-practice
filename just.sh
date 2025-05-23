d840f3e3deb913777860dba0a6a855e4a85430c3
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 15:37:00 +0530

    conversation

--- just.sh
+++ just.sh
@@ -33,9 +33,9 @@ read -p "Please enter a number : " number
 if [ $number -gt 0 ]
 then
     echo " The number is positive "
-elif [ $number -lt 0]    
+elif [ $number -lt 0 ]    
 then
     echo " The number is negative "
 else
-    echo " The number is zero "
+    echo " The number is zero " 
 fi        
