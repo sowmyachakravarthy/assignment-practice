9a68aef5a1bb04387fd91ee04f01f4d4f3e37096
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 12:55:07 +0530

    conversation

--- just.sh
+++ just.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-read -p 'Enter the name : ' name
+# read -p 'Enter the name : ' name
 
-echo 'The name you entered is : $name '
+# echo "The name you entered is : $name "
+
+number1= "$[@]"
+number2= "$[@]"
+
+echo " Sum of 2 numbers is $number1 + $number2 "
+
