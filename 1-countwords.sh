44996a22039056dc2ee5849b6e26e970fc87d541
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 12:12:04 +0530

    conversation

--- 1-countwords.sh
+++ 1-countwords.sh
@@ -3,7 +3,9 @@
 read -p "Enter filename: " file
 
 if [[ -f "$file" ]]; then
-    cat "$file" | tr '[:upper:]' '[:lower:]' | tr -s '[:space:]' '\n' | sort | uniq -c
+    cat "$file" | tr -s '[:space:]' '\n' | sort | uniq -c
 else
     echo "File does not exist."
 fi 
+
+#| tr '[:upper:]' '[:lower:]'
