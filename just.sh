832a3adb85f4933c79a8c263d961efde62806344
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 13:15:04 +0530

    conversation

--- just.sh
+++ just.sh
@@ -4,10 +4,14 @@
 
 # echo "The name you entered is : $name "
 
-read -p "enter number1 : " number1
-read -p "enter number2 : " number2
+# read -p "enter number1 : " number1
+# read -p "enter number2 : " number2
 
-total=$(($number1 + $number2))
+# total=$(($number1 + $number2))
 
-echo " Sum of 2 numbers is : $total "
+# echo " Sum of 2 numbers is : $total "
 
+
+read -p "Enter which city you live in : " city
+
+echo " Oh great, You live in $city"
