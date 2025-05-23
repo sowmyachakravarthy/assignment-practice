68d3413b4ef6fc5de67780b5d0d14b5c9830f0c2
sowmya <sowmyachakravarthy6@gmail.com>
2025-05-23 13:01:48 +0530

    conversation

--- just.sh
+++ just.sh
@@ -4,10 +4,10 @@
 
 # echo "The name you entered is : $name "
 
-number1= "30"
-number2= "50"
+number1="30"
+number2="50"
 
-total="$number1 + $number2"
+total=$(($number1 + $number2))
 
 echo " Sum of 2 numbers is : ($total) "
 
